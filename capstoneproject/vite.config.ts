f70fa9e86ee144e6272a72a6c7800858e135afbc
dxbenson252 <dxbenson252@gmail.com>
2025-10-13 18:55:55 +0000

    feat/devbenson: devcontainer updates and fixed config for port loop back

--- capstoneproject/vite.config.ts
+++ capstoneproject/vite.config.ts
@@ -3,6 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+
+
 export default defineConfig({
+  server:{
+    host: "0.0.0.0",
+  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
