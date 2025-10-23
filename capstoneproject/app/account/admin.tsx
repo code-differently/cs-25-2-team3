7f59bd1325032173e843181f8e2ad2824d9feb91
Tyran Rice Jr. <122318296+RiceViz@users.noreply.github.com>
2025-10-23 08:02:04 -0400

    chore: Deletes capstoneproject/app/account/admin.tsx

--- capstoneproject/app/account/admin.tsx
+++
@@ -1,5 +0,0 @@
-//Admin have the abilities to edit and delete forums
-//Admin information includes (id, username, role,...)
-//Admin should be able to interact the same way users would but with
-//the abilities to also mess with other users and admin post
-import React, { useState } from 'react';
